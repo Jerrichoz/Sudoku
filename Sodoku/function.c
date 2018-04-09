de207fb2f43cec1dafdf7ee3c75985e4325bf3ef
JanPHamm <38206920+JanPHamm@users.noreply.github.com>
2018-04-10 00:26:26 +0200

    Add files via upload

--- Sodoku/function.c
+++ Sodoku/function.c
@@ -9,27 +9,38 @@ int GetField(int xCoordinate, int yCoordinate, int Array[9][9])
     return Array[xCoordinate][yCoordinate];
 }
 
-int GenerateField(int Array[8][8])
+int GenerateField(int Array[9][9])
 {
+    printf("Test5\n");
     int i = 0;
     int j = 0;
-    printf("+---+---+---+\n");
-    for(i = 1; i <= 9; i++)
+    //Gitterkonstrukt - Anfang
+    printf("+-------+-------+-------+\n");
+    for(i = 0; i <= 8; i++)
     {
         printf("|");
-        for(j = 1; j <= 9; j++)
+        for(j = 0; j <= 8; j++)
         {
-            printf("%i",Array[j][i]);
-            if((j%3) == 0)
+            if (Array[j][i] == 0)
             {
-                printf("|");
+                printf(" *");
+            }
+            else
+            {
+              printf(" %i",Array[j][i]);
+            }
+            if((j==2)||(j==5))
+            {
+                printf(" |");
             }
         }
-        printf("\n");
-        if((i%3) == 0)
+        printf(" |\n");
+        if((i==2)||(i==5))
         {
-            printf("+---+---+---+\n");
+            //Gitterkonstrukt - Ende
+            printf("+-------+-------+-------+\n");
         }
     }
+    printf("+-------+-------+-------+\n");
     return 0;
 }
